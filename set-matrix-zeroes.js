17c46cf3a9ab8cb5f54d7626ed856545f0341e10
Ryan Oh <ryanohnoes@gmail.com>
2022-10-03 11:06:36 -0700

    add correct solution to set matrix zeroes

--- set-matrix-zeroes.js
+++ set-matrix-zeroes.js
@@ -1,35 +1,50 @@
+// 73. Set Matrix Zeroes
+
 var setZeroes = function(matrix) {
+    let zerosPos = []
     for(let i = 0; i < matrix.length;i++){
-        for(let j = 0; j < matrix[i].length;j++){
-            let current = matrix[i][j]
-            if(current === 0) helper(i,j,matrix)
+        for(let j = 0; j < matrix[i].length; j++){
+            if(!matrix[i][j]) zerosPos.push([i,j])
         }
     }
-    for(let i = 0; i < matrix.length;i++){
-        for(let j = 0; j < matrix[i].length;j++){
-            if(matrix[i][j] === "X") matrix[i][j] = 0
+    
+    for(let i = 0; i < zerosPos.length;i++){
+        let [row,col] = zerosPos[i]
+        for(let j = 0; j < matrix[0].length; j++){
+            matrix[row][j] = 0
+        }
+        for(let k=0; k < matrix.length; k++){
+            matrix[k][col] = 0 
         }
     }
     return matrix
-};
+}
 
-function helper(i,j,matrix){
-    if(i >= matrix.length) return
-    if(i < 0) return 
-    if(j < 0) return
-    if(j >= matrix[0].length) return
-
-    if(matrix[i][j] === 0){
-        if(i + 1 < matrix.length) matrix[i+1][j] = 'X'
-        if(i - 1 >= 0) matrix[i-1][j] = 'X'
-        if(j - 1 <= 0) matrix[i][j-1] = 'X'
-        if(j + 1 < matrix[0].length) matrix[i][j+1] = 'X'
+var setZeroes2 = function(matrix) {
+    let row = matrix.length, col = matrix[0].length, isCol = false
+    for(let i = 0; i < row;i++){
+        if(!matrix[i][0]) isCol = true
+        for(let j = 1; j < col;j++){
+            if(!matrix[i][j]){
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+            }
+        }
     }
-    if(matrix[i][j] === "X"){
-        
+    for(let i = 1; i < row;i++){
+        for(let j = 1; j < col;j++){
+            if(!matrix[i][0] || !matrix[0][j]) matrix[i][j] = 0
+        }
     }
-    helper(i+1,j,matrix)
-    helper(i-1,j,matrix)
-    helper(i,j+1,matrix)
-    helper(i,j-1,matrix)
+    if(!matrix[0][0]){
+        for(let i = 0; i < col;i++){
+            matrix[0][i] = 0
+        }
+    }
+    if(isCol){
+        for(let i = 0; i < row; i++){
+            matrix[i][0] = 0
+        }
+    }
+    return matrix
 }
